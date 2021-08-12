ca4df917317c520a2d70b71583613f71f8fead5b
suvarnalaxmi <suvarnalaxmi25@gmail.com>
2021-08-12 12:03:05 +0530

    Arithmetic examples

---
+++ first.py
@@ -0,0 +1,19 @@
+x=20
+
+z=10
+
+print("x = ",x)
+
+print("z = ",z)
+
+print("\n20+10 = ",x+z)
+
+print("\n20-10 = ",x-z)
+
+print("\n20*10 = ",x*z)
+
+print("\n20/10 = ",x/z)
+
+print("\n20**10 = ",pow(x,z))
+
+print("\n20%10 = ",x%z)
