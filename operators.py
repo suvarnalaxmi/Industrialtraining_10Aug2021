11a534f1c00d08c8a427bf5b9a38d5feeb904448
suvarnalaxmi <suvarnalaxmi25@gmail.com>
2021-08-13 12:54:45 +0530

    Operators

---
+++ operators.py
@@ -0,0 +1,38 @@
+#Arithmetic operators
+a=20
+b=10
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a/b)
+print("a mod b=",a%b)
+print("a**b=",a**b)
+print("a//b=",a//b)
+
+#Comparison operators
+print("a>b",a>b)
+print("a<b",a<b)
+print("a==b",a==b)
+print("a!=b",a!=b) 
+
+#Logical operators
+x=True
+y=False
+
+print("x and y is",x and y)
+print("x or y is",x or y)
+print("x not y is",not y)
+
+#Identity operators
+i = ["python"]
+j = ["python", "training"]
+
+print("i is j",i is j)
+print("i is not j", i is not j)
+
+#Membership operators
+m1="hello"
+m2="python,training"
+print("h in m1","h" in m1)
+print("python not in m2", "python" not in m2)
+print("python in m2", "python" in m2)
